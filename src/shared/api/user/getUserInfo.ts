82fdcf925a82edd62ae5c1e608747955d9f7b7dc
farikkm <farikk.v.2@gmail.com>
2025-04-24 23:30:54 +0500

    fix

--- src/shared/api/user/getUserInfo.ts
+++ src/shared/api/user/getUserInfo.ts
@@ -1,9 +1,10 @@
 import { UserInfo } from "shared/types";
 import { safeFetch } from "shared/api";
+import { getToken } from "shared/lib/token";
 
 export const getUserInfo = async (): Promise<UserInfo> => {
+  const token = getToken();
   const url = import.meta.env.VITE_API_BASE_URL;
-  const token = localStorage.getItem("token");
 
   try {
     const result = await safeFetch(`${url}/api/user`, {
