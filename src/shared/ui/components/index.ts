28103d5240eae60863d99494e0967ea186c86ea6
farikkm <farikk.v.2@gmail.com>
2025-05-02 15:23:15 +0500

    lib update

--- src/shared/ui/components/index.ts
+++ src/shared/ui/components/index.ts
@@ -2,3 +2,5 @@ export * from "./MyCard";
 export * from "./LoadingSpinner"
 export * from "./ThemeToggleButton";
 export * from "./AuthorizedUserCard";
+export * from "./Logo"
+export * from "./UserProfileIcon"
