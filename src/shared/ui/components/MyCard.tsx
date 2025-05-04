a1edc4c001be3b243eb57c5044419fb0ec0e0ac1
farikkm <farikk.v.2@gmail.com>
2025-05-04 15:38:35 +0500

    antd typography

--- src/shared/ui/components/MyCard.tsx
+++ src/shared/ui/components/MyCard.tsx
@@ -1,15 +1,23 @@
-import { Card } from "antd";
+import { Card, Typography } from "antd";
 import { NoteType } from "shared/types";
 import UpdateNoteModal from "widgets/notes/UpdateNoteModal";
 import { ShowModalInfo } from "widgets/notes/ShowNoteInfoModal";
 import { DeleteNotePop } from "widgets/notes/DeleteNotePop";
 
-export const MyCard: React.FC<Pick<NoteType, "content" | "title" | "_id">> = ({ _id, content, title }) => {
+const { Title, Paragraph } = Typography;
+
+export const MyCard: React.FC<Pick<NoteType, "content" | "title" | "_id">> = ({
+  _id,
+  content,
+  title,
+}) => {
   return (
     <Card
       title={
         <div className="flex justify-between items-center gap-2">
-          <h3 className="truncate">{title}</h3>
+          <Title level={5} style={{ margin: 0 }} className="truncate">
+            {title}
+          </Title>
           <div className="flex gap-3">
             <ShowModalInfo title={title} content={content} />
             <UpdateNoteModal title={title} content={content} noteId={_id} />
@@ -22,7 +30,7 @@ export const MyCard: React.FC<Pick<NoteType, "content" | "title" | "_id">> = ({ 
       hoverable
       className="select-none"
     >
-      <p>{content}</p>
+      <Paragraph ellipsis={{ rows: 3 }}>{content}</Paragraph>
     </Card>
   );
 };
