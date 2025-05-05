2059e202e1fa6a7d35be56c0a986f007a2625de1
farikkm <farikk.v.2@gmail.com>
2025-05-05 15:32:02 +0500

    AuthorizedUserCard is moved to separate widget

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,16 @@
 import { LoginForm } from "widgets/auth";
 import { AuthLayout } from "shared/ui/layout";
-import { useNavigate } from "react-router-dom";
-import { useAuthorizedUser } from "features/Auth/hooks";
 import { useTranslation } from "react-i18next";
+import AuthorizedUserCard from "widgets/user/AuthorizedUserCard";
 
 const Login = () => {
-  // Hooks
-  const navigate = useNavigate();
-  const userInfo = useAuthorizedUser();
   const { t } = useTranslation();
-
-  // Handlers
-  const handleCardClick = async () => {
-    navigate("/notes");
-  };
 
   return (
     <>
-      <AuthLayout
-        email={userInfo ? userInfo.email : ""}
-        handleCardClick={handleCardClick}
-        title={t("login.title")}
-        description={t("login.description")}
-      >
+      <AuthLayout title={t("login.title")} description={t("login.description")}>
         <LoginForm />
+        <AuthorizedUserCard />
       </AuthLayout>
     </>
   );
--- src/shared/ui/layout/AuthLayout.tsx
+++ src/shared/ui/layout/AuthLayout.tsx
@@ -1,18 +1,13 @@
 import { motion } from "motion/react";
-import { AuthorizedUserCard } from "shared/ui/components";
 
 export const AuthLayout = ({
-  email,
   title,
   description,
   children,
-  handleCardClick,
 }: {
-  email?: string;
   title: string;
   description: string;
   children: React.ReactNode;
-  handleCardClick?: () => void;
 }) => {
   return (
     <div className="auth-wrapper">
@@ -29,21 +24,6 @@ export const AuthLayout = ({
           </div>
         </motion.div>
       </div>
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
-        className="w-full max-w-xl"
-      >
-        {email ? (
-          <AuthorizedUserCard
-            handleClick={handleCardClick || (() => {})}
-            email={email}
-          />
-        ) : (
-          <div className="h-20 bg-muted rounded-xl" />
-        )}
-      </motion.div>
     </div>
   );
 };
