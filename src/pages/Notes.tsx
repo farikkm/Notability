f87d19cc03a6626457a6664eadbe78d5b1a88b1b
farikkm <farikk.v.2@gmail.com>
2025-05-05 13:26:27 +0500

    fix

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -2,7 +2,7 @@ import { AddNoteModal } from "widgets/notes";
 import { NotesList } from "entities/Notes/ui";
 import { useLoadNotes } from "features/Notes/hooks";
 import { useNotesStore } from "entities/Notes/model";
-import { LoadingSpinner, Logo, UserProfileIcon } from "shared/ui/components";
+import { LoadingSpinner, Logo } from "shared/ui/components";
 import { LoginButton } from "features/Auth/ui";
 import { useClearNotesOnUnmount } from "features/Notes/hooks/useClearNotesOnUnmount";
 
@@ -18,14 +18,15 @@ const Notes = () => {
 
   return (
     <div className={`p-5 container mx-auto`}>
-      <div className="w-full flex justify-between items-center">
-        <LoginButton />
+      <LoginButton />
+      <div className="w-fit mx-auto">
         <Logo />
-        <UserProfileIcon />
       </div>
 
       <div className="w-full flex justify-between my-6">
-        <h1 className="text-center font-bold text-2xl dark:text-white">Notes</h1>
+        <h1 className="text-center font-bold text-2xl dark:text-white">
+          Notes
+        </h1>
         <AddNoteModal />
       </div>
 
