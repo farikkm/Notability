689fe03523326ca52fd82a41c99d7b7200f0f763
farikkm <farikk.v.2@gmail.com>
2025-05-05 16:45:04 +0500

    fix

--- src/features/Auth/ui/LoginButton.tsx
+++ src/features/Auth/ui/LoginButton.tsx
@@ -1,11 +1,13 @@
 import { ArrowLeft } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const LoginButton = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === "/login" ? true : false;
+
   return (
-    <Link to={"/login"} className="icon-text-wrapper">
+    <Link to={"/login"} className={`icon-text-wrapper ${isLogin ? "pointer-events-none opacity-50 cursor-not-allowed" : ""}`}>
       <ArrowLeft />
-      <span>Login</span>
     </Link>
   );
 };
