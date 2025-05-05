90dfe1e2eec2403d9ef03cb4f09062028ec34cf8
farikkm <farikk.v.2@gmail.com>
2025-05-05 13:26:50 +0500

    now ui has only ui

--- src/shared/ui/components/UserProfileIcon.tsx
+++ src/shared/ui/components/UserProfileIcon.tsx
@@ -1,38 +1,11 @@
+import { BookUser } from "lucide-react";
 import { Dropdown, MenuProps } from "antd";
-import { useUserStore } from "entities/User/model";
-import { BookUser, LogOut, Settings } from "lucide-react";
-import { useNavigate } from "react-router-dom";
 
-export const UserProfileIcon = () => {
-  const navigate = useNavigate();
-  const logout = useUserStore((state) => state.logout);
+interface Props {
+  items: MenuProps["items"];
+}
 
-  const handleLogout = () => {
-    logout();
-    navigate("/login", { replace: true });
-  };
-
-  const items: MenuProps["items"] = [
-    {
-      label: "note2@note.com",
-      key: "1",
-      danger: true,
-      disabled: true,
-    },
-    {
-      label: "Settings",
-      key: "2",
-      icon: <Settings />,
-    },
-    {
-      label: "Logout",
-      key: "3",
-      icon: <LogOut />,
-      danger: true,
-      onClick: handleLogout
-    },
-  ];
-
+export const UserProfileIcon = ({ items }: Props) => {
   return (
     <Dropdown placement="bottomRight" menu={{ items }} trigger={["click"]}>
       <a onClick={(e) => e.preventDefault()}>
